2c1894a481b28bfd191d4fefff3ddb3a79a706c7
Olivier Della Maggiore <odellamaggiore@localhost.localdomain>
2019-04-01 13:11:58 +0200

    update path rest api

--- src/main/java/ch/serial/game/skipass/infrastructure/rest/SkiPassRestImpl.java
+++ src/main/java/ch/serial/game/skipass/infrastructure/rest/SkiPassRestImpl.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ch.serial.game.skipass.application.SkiPassService;
 
 @RestController
-@RequestMapping("forfaits")
+@RequestMapping("rest/api/v1/forfaits")
 public class SkiPassRestImpl implements SkiPassRest {
 	
 	@Inject
